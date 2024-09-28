d882c42f40f453e6ab8bf9947cb761de621e8fb6
Julio Mendes <juliomendes160@hotmail.com>
2024-09-28 08:19:23 -0300

    3.4 Tipos e Variáveis - 4

--- Aprendendo a Sintaxe Java/dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ Aprendendo a Sintaxe Java/dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,9 +1,23 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
+        //tipos primitivos
+        //estudem a classe String que representa texto na aplicação
+
+        String meuNome = "GLEYSON SAMPAIO";
+
+
         double salarioMinimo = 2500;
 
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
+        
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.println(numero);
+
+        final double VALOR_DE_PI = 3.14;
     }
 }
