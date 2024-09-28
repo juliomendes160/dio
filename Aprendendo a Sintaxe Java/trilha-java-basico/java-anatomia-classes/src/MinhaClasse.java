4cff8fbea5d52a2f1f78247eac6b6c5191255cc4
Julio Mendes <juliomendes160@hotmail.com>
2024-09-28 07:19:48 -0300

    2.3 Anatomia das Classes - 3

--- Aprendendo a Sintaxe Java/trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
+++ Aprendendo a Sintaxe Java/trilha-java-basico/java-anatomia-classes/src/MinhaClasse.java
@@ -2,8 +2,16 @@ public class MinhaClasse {
 
     public static void main(String[] args) {
 
-        System.out.println("Olá turma, sejam bem-vindos");
+        String primeiroNome = "Gleyson";
+        String segundoNome = "Sampaio";
 
+        String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
+
+        System.out.println(nomeCompleto);
+    }
+
+    public static String nomeCompleto(String primeiroNome, String segundoNome){
+        return "Resultado do método "+ primeiroNome.concat(" ").concat(segundoNome);
     }
 
 }
