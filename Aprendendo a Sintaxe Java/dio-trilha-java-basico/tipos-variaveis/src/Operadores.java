f90eeb2e0e16b45e90473e23aaf1e6d5e94de6e1
Julio Mendes <juliomendes160@hotmail.com>
2024-09-28 08:56:05 -0300

    4.6 Operadores - 6

--- Aprendendo a Sintaxe Java/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
+++ Aprendendo a Sintaxe Java/dio-trilha-java-basico/tipos-variaveis/src/Operadores.java
@@ -1,13 +1,31 @@
 public class Operadores {
     public static void main(String[] args) {
-       int a, b;
-       a = 6;
-       b = 6;
+        String nomeUm = "GLEYSON";
+        String nomeDois = new String("GLEYSON");
 
-       int resultado = a==b ? 1 : 0;
+        System.out.println(nomeUm.equals(nomeDois));
 
-       
-       System.out.println(resultado);
-    
+        
+        
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if (numero1 == numero2) { // true
+            System.out.println("a nossa condição é verdadeira");
+        }
+
+        
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é diferente a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+
     }
 }
