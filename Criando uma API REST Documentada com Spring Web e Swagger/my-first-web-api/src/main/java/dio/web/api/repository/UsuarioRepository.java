cbd4d45f77124eabdb02bd73ac82ada98d3d2640
Julio Mendes <juliomendes160@hotmail.com>
2024-10-03 16:47:52 -0300

    2 4 Habilitando o tratamento de exceções de negócio com handlers

--- Criando uma API REST Documentada com Spring Web e Swagger/my-first-web-api/src/main/java/dio/web/api/repository/UsuarioRepository.java
+++ Criando uma API REST Documentada com Spring Web e Swagger/my-first-web-api/src/main/java/dio/web/api/repository/UsuarioRepository.java
@@ -1,5 +1,6 @@
 package dio.web.api.repository;
 
+import dio.web.api.handler.CampoObrigatorioException;
 import dio.web.api.model.Usuario;
 import org.springframework.stereotype.Repository;
 
@@ -9,6 +10,12 @@ import java.util.List;
 @Repository
 public class UsuarioRepository {
     public void save(Usuario usuario){
+        if(usuario.getLogin()==null)
+            throw new CampoObrigatorioException("login");
+
+        if(usuario.getPassword()==null)
+            throw new CampoObrigatorioException("password");
+
         if(usuario.getId()==null)
             System.out.println("SAVE - Recebendo o usuário na camada de repositório");
         else
