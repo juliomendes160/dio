949f9d2e7eb3af672b3472ea5ea9c21d961dbea3
Julio Mendes <juliomendes160@hotmail.com>
2024-10-05 07:54:53 -0300

    2 10 JPA Repository

--- Introdução a Spring Framework com Spring Boot/dio-spring-data-jpa/src/main/java/dio/aula/StartApp.java
+++ Introdução a Spring Framework com Spring Boot/dio-spring-data-jpa/src/main/java/dio/aula/StartApp.java
@@ -14,10 +14,20 @@ public class StartApp implements CommandLineRunner {
     private UserRepository repository;
     @Override
     public void run(String... args) throws Exception {
+        insertUser("GLEYSON", "glysns", "dio123");
+        insertUser("GLEYSON SAMPAIO", "glysns", "dio123");
+        insertUser("GABRIEL NUNES", "santos", "gabriel");
+
+        List<User> users = repository.findByNameContaining("GLEYSON");
+        for(User u: users){
+            System.out.println(u);
+        }
+    }
+    private void insertUser(String name, String username, String password){
         User user = new User();
-        user.setName("GLEYSON");
-        user.setUsername("glysns");
-        user.setPassword("dio123");
+        user.setName(name);
+        user.setUsername(username);
+        user.setPassword(password);
 
         repository.save(user);
 
