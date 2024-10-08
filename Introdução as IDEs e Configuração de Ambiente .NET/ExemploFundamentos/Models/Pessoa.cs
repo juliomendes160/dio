b91b12db0a5245523ef9854b3e067c8302f2cb4a
Julio Mendes <juliomendes160@hotmail.com>
2024-10-08 14:47:32 -0300

    1.4 Entendendo a estrutura do código

--- Introdução as IDEs e Configuração de Ambiente .NET/ExemploFundamentos/Models/Pessoa.cs
+++ Introdução as IDEs e Configuração de Ambiente .NET/ExemploFundamentos/Models/Pessoa.cs
@@ -10,8 +10,9 @@ namespace ExemploFundamentos.Models
         public string Nome { get; set; }
         public int Idade { get; set; }
 
-        public void Apresentar()
-        {
+        public void Apresentar(string @class)
+        {   
+            @class = "teste";
             Console.WriteLine($"Olá, meu nome é {Nome}, e tenho {Idade} anos");
         }
     }
