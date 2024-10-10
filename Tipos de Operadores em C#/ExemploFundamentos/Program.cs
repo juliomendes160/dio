c4e4cddaa723b04f506cdc4902c9dee0e646e113
Julio Mendes <juliomendes160@hotmail.com>
2024-10-10 13:43:35 -0300

    2.4 IF aninhado

--- Tipos de Operadores em C#/ExemploFundamentos/Program.cs
+++ Tipos de Operadores em C#/ExemploFundamentos/Program.cs
@@ -1,14 +1,17 @@
 ﻿using ExemploFundamentos.Models;
 
 int quantidadeEmEstoque = 3;
-int quantidadeCompra = 4;
-bool possivelVenda = quantidadeEmEstoque >= quantidadeCompra;
+int quantidadeCompra = 0;
+bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
 
 Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
 Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
 Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
 
-if(possivelVenda){
+if(quantidadeCompra == 0) {
+    Console.WriteLine("Venda inválida.");
+}
+else if(possivelVenda){
     Console.WriteLine("Venda realizada.");
 }
 else{
--- Tipos de Operadores em C#/ExemploFundamentos/obj/Debug/net6.0/ExemploFundamentos.AssemblyInfo.cs
+++ Tipos de Operadores em C#/ExemploFundamentos/obj/Debug/net6.0/ExemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a2b9b18ac7687b59260f13b1807f813eb01bd47e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28a366ca5971e3b3302b07bb9c1aa042b64ea2f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
