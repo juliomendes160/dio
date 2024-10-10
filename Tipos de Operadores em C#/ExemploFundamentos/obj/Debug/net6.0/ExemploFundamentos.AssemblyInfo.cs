7a4fd2073b6daa04339c72f51f1a298b923c6767
Julio Mendes <juliomendes160@hotmail.com>
2024-10-10 12:46:41 -0300

    1.2 Combinando operadores

--- Tipos de Operadores em C#/ExemploFundamentos/obj/Debug/net6.0/ExemploFundamentos.AssemblyInfo.cs
+++ Tipos de Operadores em C#/ExemploFundamentos/obj/Debug/net6.0/ExemploFundamentos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+75dc2fac6fbcfe9780199b7b6c3fcf65910d9578")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92dfb99e29f6e4ada82c7ff23ea8bac6cc8b3833")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExemploFundamentos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
