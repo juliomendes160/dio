652b43262743f6298734618908152307375f67b5
Julio Mendes <juliomendes160@hotmail.com>
2024-10-10 13:50:32 -0300

    2.5 Aprendendo o switch case

--- Tipos de Operadores em C#/ExemploFundamentos/Program.cs
+++ Tipos de Operadores em C#/ExemploFundamentos/Program.cs
@@ -1,22 +1,65 @@
 ﻿using ExemploFundamentos.Models;
 
-int quantidadeEmEstoque = 3;
-int quantidadeCompra = 0;
-bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+Console.WriteLine("Digite uma letra");
+string letra = Console.ReadLine();
 
-Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
-Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
-Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+switch (letra){
+    case "a":
+    case "e":
+    case "i":
+    case "o":
+    case "u":
+        Console.WriteLine("Vogal");
+        break;
+    
+    default:
+        Console.WriteLine("Não é uma vogal");
+        break;
+}
 
-if(quantidadeCompra == 0) {
-    Console.WriteLine("Venda inválida.");
-}
-else if(possivelVenda){
-    Console.WriteLine("Venda realizada.");
-}
-else{
-    Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
-}
+// if(letra == "a" || letra == "e" || letra == "i" || letra == "o" || letra == "u"){
+//     Console.WriteLine("Vogal");
+// }
+// else{
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+// if(letra == "a"){
+//     Console.WriteLine("Vogal");
+// }
+// if(letra == "e"){
+//     Console.WriteLine("Vogal");
+// }
+// if(letra == "i"){
+//     Console.WriteLine("Vogal");
+// }
+// if(letra == "o"){
+//     Console.WriteLine("Vogal");
+// }
+// if(letra == "u"){
+//     Console.WriteLine("Vogal");
+// }
+// else{
+//     Console.WriteLine("Não é uma vogal");
+// }
+
+// int quantidadeEmEstoque = 3;
+// int quantidadeCompra = 0;
+// bool possivelVenda = quantidadeCompra > 0 && quantidadeEmEstoque >= quantidadeCompra;
+
+// Console.WriteLine($"Quantidade em estoque: {quantidadeEmEstoque}");
+// Console.WriteLine($"Quantidade compra: {quantidadeCompra}");
+// Console.WriteLine($"É possível realizar a venda? {possivelVenda}");
+
+// if(quantidadeCompra == 0) {
+//     Console.WriteLine("Venda inválida.");
+// }
+// else if(possivelVenda){
+//     Console.WriteLine("Venda realizada.");
+// }
+// else{
+//     Console.WriteLine("Desculpe. Não temos a quantidade desejada em estoque.");
+// }
 
 // string a = "15-";
 
