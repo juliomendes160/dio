92dfb99e29f6e4ada82c7ff23ea8bac6cc8b3833
Julio Mendes <juliomendes160@hotmail.com>
2024-10-10 12:41:59 -0300

    1.1 Introdução operador de atribuição

---
+++ Tipos de Operadores em C#/ExemploFundamentos/Program.cs
@@ -0,0 +1,39 @@
+﻿using ExemploFundamentos.Models;
+
+int a = 10;
+int b = 20;
+
+int c = a + b;
+
+Console.WriteLine(c);
+
+// DateTime dataAtual = DateTime.Now.AddDays(5);
+// Console.WriteLine(dataAtual.ToString("dd/MM/yyyy HH:mm"));
+
+// string apresentacao = "Olá, seja bem vindo";
+
+// int quantidade = 1;
+// Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+
+// quantidade = 10;
+// Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+
+// double altura = 1.80;
+
+// decimal preco = 1.80M;
+
+// bool condicao = true;
+
+// Console.WriteLine(apresentacao);
+// Console.WriteLine("Valor da variável quantidade: "+ quantidade);
+// Console.WriteLine("Valor da variável altura: "+ altura.ToString("0.00"));
+// Console.WriteLine("Valor da variável preco: "+ preco);
+// Console.WriteLine("Valor da variável condicao: "+ condicao);
+
+// Pessoa pessoa1 = new Pessoa();
+
+// pessoa1.Nome = "Buta";
+// pessoa1.Idade = 26;
+// pessoa1.Apresentar();
+
+// Pessoa _pessoa_FisicaRepresentacao = new Pessoa();
