6b03569e1b4c28a34a55f0ff56d28ce587de56f1
Julio Mendes <juliomendes160@hotmail.com>
2025-01-10 13:16:33 -0300

    1.3 Tipos de Dados e Declaração de Variáveis

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,3 +1,12 @@
 void main(List<String> arguments) {
-  print('Hello world: ${meu_app.calculate()}!');
+  int numero1 = 10;
+  var numero2 = 11;
+
+  print("Retorna verdadeiro se somente se esse inteiro for par");
+  print(numero1.isEven);
+  print(numero2.isEven);
+
+  print("Retorna verdadeiro se somente se este inteiro for ímpar");
+  print(numero1.isOdd);
+  print(numero2.isOdd);
 }
