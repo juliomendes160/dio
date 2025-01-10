91b685275fff7819be9ee2289e76e9910b69635a
Julio Mendes <juliomendes160@hotmail.com>
2025-01-10 13:44:50 -0300

    1.5 Tipos de Dados Double

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,30 +1,41 @@
 void main(List<String> arguments) {
-  int numero1 = 10;
-  var numero2 = 11;
+  double numero1 = 10.9;
+  var numero2 = 11.1;
 
-  print("Retorna verdadeiro se somente se esse inteiro for par");
-  print(numero1.isEven);
-  print(numero2.isEven);
+  print("Remover ponto flutuante");
+  print(numero1.truncate());
+  print(numero2.truncate());
 
-  print("Retorna verdadeiro se somente se este inteiro for ímpar");
-  print(numero1.isOdd);
-  print(numero2.isOdd);
+  print("Converter para inteiro.");
+  print(numero1.toInt());
+  print(numero2.toInt());
+
+  print("Arredonda para cima.");
+  print(numero1.ceil());
+  print(numero2.ceil());
+
+  print("Arredonda para baixo.");
+  print(numero1.floor());
+  print(numero2.floor());
 
   print("Retorna se o número é finito");
   print(numero1.isFinite);
-  
+  print(numero2.isFinite);
+
   print("Retorna se o número é infinito");
-  print(double.infinity);
+  print(numero1.isInfinite);
+  print(numero2.isInfinite);
 
   print("Retorna se o número não é um número válido");
   print(numero1.isNaN);
+  print(numero2.isNaN);
   
   print("Retorna se o número é negativo");
   print(numero1.isNegative);
   print((numero2 * -1).isNegative);
 
   print("Converter String para inteiro");
-  print(int.parse("10"));
+  print(double.parse("123.456"));
   // print(int.parse("teste"));
   print(int.tryParse("teste"));
 }
