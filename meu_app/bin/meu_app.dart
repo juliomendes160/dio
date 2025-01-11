8b12cefae24f66468538ec8e75fee91ec723dbf7
Julio Mendes <juliomendes160@hotmail.com>
2025-01-11 12:49:47 -0300

    1.11 Tipos de Dados Dinâmico

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,6 +1,15 @@
 void main(List<String> arguments) {
-  const String variavel = "ABC";
-  const int variavel1 = 1;
-  const bool variavel2 = true;
-  print(variavel);
+  dynamic var1 = "ABC";
+  var var2 = "ABC";
+  print(var2.length);
+  var1 = 10;
+  print(var1);
+  var1 = 99.54;
+  print(var1);
+  var1 = true;
+  print(var1);
+  var1 = DateTime(2022, 01, 02);
+  print(var1);
+  var1 = [10, true, "A"];
+  print(var1);
 }
