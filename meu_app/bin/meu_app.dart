df08d3567118dc996663ae02c2a407ef15401c0a
Julio Mendes <juliomendes160@hotmail.com>
2025-01-11 11:22:14 -0300

    1.8 Tipos de Dados List

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,10 +1,49 @@
 void main(List<String> arguments) {
-  var varTrue = true;
-  bool varFalse = false;
+  List<String> lstString = [];
+  var lstInt = [1, 10, 50];
+  List lstDynamic = [];
 
-  print(varTrue);
-  print(varFalse);
-  print(!varTrue);
-  print(!varFalse);
-  print(varTrue == varFalse);
+  print("Tamanho da Lista");
+  print(lstString.length);
+  print(lstInt.length);
+  print(lstDynamic.length);
+
+  print(lstString);
+  print(lstInt);
+  print(lstDynamic);
+
+  print("Adicionar item");
+  lstString.add("M");
+  lstString.add("B");
+  lstInt.add(90);
+  lstDynamic.add("A");
+  lstDynamic.add(10);
+  lstDynamic.add(9.5);
+  lstDynamic.add(true);
+
+  print(lstString);
+  print(lstInt);
+  print(lstDynamic);
+
+  print("Remover item");
+  lstString.remove("M");
+  lstInt.remove(90);
+  lstDynamic.remove(9.5);
+
+  print(lstString);
+  print(lstInt);
+  print(lstDynamic);
+
+  print("Verifica se a lista está ou não vazia");
+  print(lstString.isEmpty);
+  print(lstInt.isNotEmpty);
+
+
+  print("Ver valor contém na lista");
+  print(lstString.contains("C"));
+  print(lstDynamic.contains(false));
+
+  print("Função where");
+  print(lstInt);
+  print(lstInt.where((x) => x > 9 && x < 99));
 }
