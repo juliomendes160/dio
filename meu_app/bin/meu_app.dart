decb8ea586935c2c6acea61a758abe3955472826
Julio Mendes <juliomendes160@hotmail.com>
2025-01-13 13:55:57 -0300

    1.5 Criando uma Calculadora

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -2,23 +2,33 @@ import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> arguments) {
-  print("Informe a primeira nota:");
+  print("Bem vindos a nossa calculadora!");
+
+  print("Informe o primeiro número:");
   var line = stdin.readLineSync(encoding: utf8);
-  var prova1 = int.parse(line ?? "0");
+  var numero1 = double.parse(line ?? "0");
 
-  print("Informe a segunda nota:");
+  print("Informe o segundo número:");
   line = stdin.readLineSync(encoding: utf8);
-  var prova2 = int.parse(line ?? "0");
+  var numero2 = double.parse(line ?? "0");
 
-  var media = (prova1 + prova2) / 2;
-  if (media >= 7) {
-    print("O aluno passou com a nota: $media");
-  } else if (media >= 5) {
-    print("O aluno está de recuperação com a nota: $media");
+  print("Informe a operação matemática (+, -, *, /):");
+  line = stdin.readLineSync(encoding: utf8);
+  var operacao = line ?? "";
+
+  double resultado = 0;
+  if (operacao == "+") {
+    resultado = numero1 + numero2;
+  } else if (operacao == "-") {
+    resultado = numero1 - numero2;
+  } else if (operacao == "*") {
+    resultado = numero1 * numero2;
+  } else if (operacao == "/") {
+    resultado = numero1 / numero2;
   } else {
-    print("O aluno reprovou com a nota: $media");
+    print("Operação inválida!");
+    exit(0);
   }
-
-  var resultado = (prova1 >= 7) ? "O aluno passou" : "O aluno reprovou";
-  print(resultado);
+  print("Operação solicitada: $operacao");
+  print("O resultado da operação é: $resultado");
 }
