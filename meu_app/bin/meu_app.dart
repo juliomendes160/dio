9548bc11f773038ead0b4db0df6bf5178bdfa11e
Julio Mendes <juliomendes160@hotmail.com>
2025-01-13 14:47:41 -0300

    1.10 Laços de Repetição DO_WHILE

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -2,21 +2,16 @@ import 'dart:convert';
 import 'dart:io';
 
 void main(List<String> arguments) {
-  var contador = 0;
-  var total = 10;
-  while (contador < total) {
-    contador = contador + 1;
-    print(contador);
-  }
-
-  print("Digite um número ou 'S' para sair");
-  var line = stdin.readLineSync(encoding: utf8);
-  double acumulador = 0;
-  while (line != "S") {
-    var numero = double.parse(line ?? "");
-    acumulador = acumulador + numero;
+  var opcao = "";
+  var acumulador = 0.0;
+  do {
     print("Digite um número ou 'S' para sair");
-    line = stdin.readLineSync(encoding: utf8);
-  }
+    var line = stdin.readLineSync(encoding: utf8);
+    opcao = line ?? "";
+    var numero = double.tryParse(opcao);
+    if (numero != null) {
+      acumulador = acumulador + numero;
+    }
+  } while (opcao != "S");
   print(acumulador);
 }
