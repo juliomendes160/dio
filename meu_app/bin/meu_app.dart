6401aa063cc3031e759ee493c30d8adb7ebeeb83
Julio Mendes <juliomendes160@hotmail.com>
2025-01-13 13:15:02 -0300

    1.2 Condicional IF

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,12 +1,12 @@
 void main(List<String> arguments) {
-  var prova1 = 10;
+  var prova1 = 9;
   var prova2 = 5;
   var media = (prova1 + prova2) / 2;
-  print(media);
-  print(media >= 7 );
-  print(media < 7);
-  print(prova1 == prova2);
-  print(prova1 != prova2);
-  print((prova1 >= 7) && (prova2 >= 7));
-  print((prova1 >= 10) || (prova2 >= 10));
+  if (media >= 7 ) {
+    print("O aluno passou com a nota: $media");
+  } else if (media >= 5) {
+    print("O aluno está de recuperação com a nota: $media");
+  } else {
+    print("O aluno reprovou com a nota: $media");
+  }
 }
