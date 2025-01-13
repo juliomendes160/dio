6064af91f7f279d06fb917bbefec4ee2f125a281
Julio Mendes <juliomendes160@hotmail.com>
2025-01-13 13:30:29 -0300

    1.4 Lendo Dados do Console

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,8 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+
 void main(List<String> arguments) {
-  var prova1 = 9;
-  var prova2 = 5;
+  print("Informe a primeira nota:");
+  var line = stdin.readLineSync(encoding: utf8);
+  var prova1 = int.parse(line ?? "0");
+
+  print("Informe a segunda nota:");
+  line = stdin.readLineSync(encoding: utf8);
+  var prova2 = int.parse(line ?? "0");
+
   var media = (prova1 + prova2) / 2;
-  if (media >= 7 ) {
+  if (media >= 7) {
     print("O aluno passou com a nota: $media");
   } else if (media >= 5) {
     print("O aluno está de recuperação com a nota: $media");
