452c210c8627c141b430478c296790aac10757cf
Julio Mendes <juliomendes160@hotmail.com>
2025-01-13 13:05:30 -0300

    1.1 Introdução

--- meu_app/bin/meu_app.dart
+++ meu_app/bin/meu_app.dart
@@ -1,13 +1,12 @@
 void main(List<String> arguments) {
-  DateTime data1 = DateTime.now();
-  data1 = DateTime.parse("2022-02-01 11:28:57");
-
-  // Partes da data
-  print(data1);
-  print(data1.day);
-  print(data1.month);
-  print(data1.year);
-  print(data1.hour);
-  print(data1.minute);
-  print(data1.second);
+  var prova1 = 10;
+  var prova2 = 5;
+  var media = (prova1 + prova2) / 2;
+  print(media);
+  print(media >= 7 );
+  print(media < 7);
+  print(prova1 == prova2);
+  print(prova1 != prova2);
+  print((prova1 >= 7) && (prova2 >= 7));
+  print((prova1 >= 10) || (prova2 >= 10));
 }
