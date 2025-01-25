cabe50b5c6e102f476f8f85c7cd5fb6db260fd12
Juliermes Mendes <juliomendes160@hotmail.com>
2025-01-25 13:15:40 -0300

    Final

---
+++ .vscode/.cmd
@@ -0,0 +1,5 @@
+@echo off
+set git=C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin
+set code=C:\Program Files\Microsoft VS Code\bin
+set system=C:\WINDOWS\system32;C:\WINDOWS\System32\WindowsPowerShell\v1.0
+set PATH=%flutter%;%git%;%code%;%system%;
