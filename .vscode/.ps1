9e4c5ca05046d8b8eb5a0dc13647fe91ec0d9d18
Julio Mendes <juliomendes160@hotmail.com>
2025-02-28 16:39:56 -0300

    Initial commit

---
+++ .vscode/.ps1
@@ -0,0 +1,5 @@
+$env:flutter="D:\Dev\Tools\flutter\flutter-3.0.5\bin"
+$env:git="C:\Program Files\Git\mingw64\bin;C:\Program Files\Git\usr\bin"
+$env:code="C:\Program Files\Microsoft VS Code\bin"
+$env:system="C:\WINDOWS\system32;C:\WINDOWS\System32\WindowsPowerShell\v1.0"
+$env:PATH="$env:flutter;$env:git;$env:code;$env:system"
