e4d0c6a780b845579a86f040933f082dbbb39863
Julio Mendes <juliomendes160@hotmail.com>
2025-07-08 19:40:47 -0300

    docs: final commit

---
+++ setup.sh
@@ -0,0 +1,22 @@
+# chore: initial commit
+# chore: environment
+# chore: dependency
+# chore: project
+# chore: server
+# docs: final commit
+
+git clone <repository-url>
+
+git commit --allow-empty -m "<commit-message>"
+
+git submodule add --force -b <branch-name> <branch-url> <folder-name>
+
+git add .gitmodules <submodule-folder>
+
+git push --force
+
+git push --set-upstream origin <branch-name> </branch-name>
+
+git reset --hard
+
+git clean -fd
