acf654954dadce115f5e79f20f343f4273737fb9
Juliermes Mendes <juliomendes160@hotmail.com>
2026-08-29 18:55:17 -0300

    4.2 Documentando nossa API com SWAGGER

--- my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
+++ my-first-web-api/src/main/java/dio/web/api/controller/UsuarioController.java
@@ -28,4 +28,6 @@ public class UsuarioController {
     public void postUser(@RequestBody Usuario usuario) {
         repository.save(usuario);
     }
+    @PutMapping()
+    public void putUser(@RequestBody Usuario usuario) { repository.save(usuario);}
 }
